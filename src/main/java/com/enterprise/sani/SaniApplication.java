f7ec8c8a03048079f7841c9917d2ef0166008d63
marvenmalope <marvenmalope2003@gmail.com>
2024-07-26 23:57:42 +0200

    Initial commit

---
+++ src/main/java/com/enterprise/sani/SaniApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.sani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaniApplication.class, args);
+	}
+
+}
